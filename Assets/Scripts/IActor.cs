e105f96fd7381f201bebdba15d2dbe913ed82cc1
stephenhou2 <stephenhou2@163.com>
2021-08-11 21:33:44 +0800

    update

---
+++ Assets/Scripts/IActor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IActor
+{
+    int GetEntityId();
+    int GetWordId();
+
+    void OnCreate();
+    void OnDestroy();
+
+    void ReceiveEvent(GameEvent evt); //接收事件（行为）
+}
